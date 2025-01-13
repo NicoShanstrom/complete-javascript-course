bbdc2e7b7d78294c35aa4cc4227002c3a6dc1b89
Nico Shanstrom <nicoshanstrom@NicoShantii.localdomain>
2025-01-13 11:41:21 -0500

    January 13th finishing fundamentals part 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -163,3 +163,82 @@
 
 // if (favorite !== 3) console.log('Why not 3?');
 
+// // boolean logic
+// const hasDriversLicense = true; // A
+// const hasGoodVision = true; // B
+// const isTired = false; // C
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// const shouldDrive = hasDriversLicense && hasGoodVision && !isTired;
+
+// if (shouldDrive) {
+//   console.log('Sarah is able to drive!')
+// } else {
+//   console.log('Someone else should drive...');
+// }
+
+
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// // switch statements
+// const day = 'fridays';
+
+// switch (day) {
+//   case 'monday': // day === 'monday'
+//     console.log('Plan course structure');
+//     console.log('Go to coding meetup');
+//     break;
+//   case 'tuesday':
+//     console.log('Prepare theory videos');
+//     break;
+//   case 'wednesday':
+//   case 'thursday':
+//     console.log('Write code examples');
+//     break;
+//   case 'friday':
+//     console.log('Record videos');
+//     break;
+//   case 'saturday':
+//   case 'sunday':
+//     console.log('Enjoy weekend :D');
+//     break;
+//   default:
+//     console.log('Not a valid day!');
+// }
+
+// if (day === 'monday') {
+//   console.log('Plan course structure');
+//   console.log('Go to coding meetup');
+// } else if (day === 'tuesday') {
+//   console.log('Prepare theory videos');
+// } else if (day === 'wednesday' || day === 'thursday') {
+//   console.log('Write code examples');
+// } else if (day === 'friday') {
+//   console.log('Record videos');
+// } else if (day === 'saturday' || day === 'sunday') {
+//   console.log('Enjoy weekend :D');
+// } else {
+//   console.log('Not a valid day!');
+// }
+
+// // conditional ternary one line if/else
+// const age = 23;
+// // age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water');
+
+// const drink = age >= 18 ? 'wine' : 'water';
+// console.log(drink);
+
+// // normal if/else
+// let drink2;
+// if (age >= 18) {
+//   drink2 = 'wine';
+// } else {
+//   drink2 = 'water';
+// }
+// console.log(drink2);
+
+// console.log(`I like to drink ${drink}`);
+// console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
